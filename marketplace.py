3b70f56bfee330e675501262aece9dcaad883cf8
Rohan Pahwa <71563384+rohanphw@users.noreply.github.com>
2022-07-15 20:14:08 +0530

    Update marketplace.py

--- marketplace.py
+++ marketplace.py
@@ -1,13 +1,11 @@
 import smartpy as sp
 
 class marketplace(sp.Contract):
-    def __init__(self,admin, metadata, royaltyPercentage, token):
+    def __init__(self,admin, metadata, royaltyPercentage):
         self.init(
-            token = token,
             metadata = metadata,
             admin = admin,
             royaltyPercentage=royaltyPercentage,
-            listings= sp.map(listingId=sp.TNat, tvalue=sp.TRecord(amount=sp.TNat, tokenId=sp.TNat, seller=sp.TAddress, price_per_unit=sp.TMutez, active=sp.bool)),
-            auctions= sp.map(auctionId=sp.TNat, tvalue=sp.Trecord(amount=sp.Tnat, tokenId=sp.TNat, seller=sp.TAddress, timePeriod=sp.timestamp)),
-            
+            listings= sp.map(listingId=sp.TNat, tvalue=sp.TRecord(amount=sp.TNat, token=sp.TAddress, tokenId=sp.TNat, seller=sp.TAddress, price_per_unit=sp.TMutez, active=sp.bool)),
+            auctions= sp.map(auctionId=sp.TNat, tvalue=sp.Trecord(amount=sp.Tnat, token=sp.TAddress, tokenId=sp.TNat, seller=sp.TAddress, timePeriod=sp.timestamp)),    
         )
