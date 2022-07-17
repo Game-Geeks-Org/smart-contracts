97f03267c308a6e0a8c65e4774082e6c0938195e
Priyam Anand <priyam27.anand@gmail.com>
2022-07-17 18:33:33 +0530

    marketplace : added auction related features, added tests for the same

--- marketplace.py
+++ marketplace.py
@@ -7,6 +7,21 @@ class Error_message :
     def invalidRoyalty(self):
         return "INVALID_ROYALTY"
 
+    def invalidTimePeriod(self):
+        return "INVALID_TIME_PERIOD"
+
+    def invalidPrice(self):
+        return "INVALID_PRICE"
+
+    def invalidAuction(self):
+        return "INVALID_AUCTION"
+
+    def invalidBid(self):
+        return "INVALID_BID"
+
+    def ownerOnly(self):
+        return "OWNER_ONLY"
+
 '''
     NOTE: 
     1. Royalty percentage is calculated as follows:
@@ -16,14 +31,14 @@ class Error_message :
     2. Division of royalty between third party nfts and admin is also carried in the same way as described above
 '''
 class marketplace(sp.Contract):
-    def __init__(self,admin, _royalty, metadata):
+    def __init__(self,admin, _royalty):
         self.error = Error_message()
         
-        self.init_type(sp.TRecord(
-            metadata=sp.TBigMap(sp.TString,sp.TBytes)
-        ))
+        # self.init_type(sp.TRecord(
+        #     metadata=sp.TBigMap(sp.TString,sp.TBytes)
+        # ))
         self.init(
-            metadata = metadata,
+            # metadata = metadata,
             listingId = sp.nat(0),
             auctionId = sp.nat(0),
             admin = admin,
@@ -37,17 +52,85 @@ class marketplace(sp.Contract):
             )),
             auctions = sp.map(tkey = sp.TNat, tvalue = sp.TRecord(
                 token = sp.TAddress, tokenId = sp.TNat, amount = sp.TNat,
-                seller = sp.TAddress, timePeriod = sp.TTimestamp
+                basePrice = sp.TMutez, seller = sp.TAddress, timePeriod = sp.TTimestamp
             )),
+            bids = sp.map(tkey = sp.TNat, tvalue = sp.TRecord(
+                value = sp.TMutez, owner = sp.TAddress
+            ))
         )
 
+    # Utility Functions
     def _onlyAdmin(self):
         sp.verify(sp.sender == self.data.admin, self.error.adminOnly())
 
     def _checkValidRoyalty(self, amount):
         sp.verify(amount <= sp.nat(1000),self.error.invalidRoyalty())
 
+    def _transferTokens(self,token, tokenId, amount, from_, to_):
+        c = sp.contract(
+                sp.TList(
+                    sp.TRecord(
+                        from_=sp.TAddress, 
+                        txs=sp.TList(
+                            sp.TRecord(
+                                amount=sp.TNat, 
+                                to_=sp.TAddress, 
+                                token_id=sp.TNat).layout(("to_", ("token_id", "amount"))
+                            )
+                        )
+                    )
+                ), 
+                token, 
+                entry_point='transfer'
+            ).open_some()
+        sp.transfer(
+            sp.list([sp.record(from_=from_, txs=sp.list([sp.record(amount=amount, to_=to_, token_id=tokenId)]))]), 
+            sp.mutez(0), 
+            c
+        )
 
+    def _refundBid(self, _auctionId):
+        amount = sp.local('amount',sp.map({},tkey = sp.TNat, tvalue = sp.TMutez))
+        to = sp.local('to', sp.map({},tkey = sp.TNat, tvalue = sp.TAddress))
+
+        sp.if self.data.bids.contains(_auctionId):
+            amount.value[0] = self.data.bids[_auctionId].value
+            to.value[0] = self.data.bids[_auctionId].owner
+            del self.data.bids[_auctionId]
+
+        sp.if amount.value.contains(0):
+            sp.send(to.value[0], amount.value[0])
+
+    def _auctionExists(self, _auctionId):
+        sp.verify(self.data.auctions.contains(_auctionId), self.error.invalidAuction())
+
+    def _isAuctionActive(self, _auctionId):
+        sp.verify(self.data.auctions[_auctionId].timePeriod > sp.now, self.error.invalidAuction())
+
+    def _isBidValid(self, _auctionid):
+        sp.if self.data.bids.contains(_auctionid):
+            sp.verify(sp.amount > self.data.bids[_auctionid].value, self.error.invalidBid())
+        sp.else:
+            sp.verify(sp.amount >= self.data.auctions[_auctionid].basePrice, self.error.invalidBid())
+
+    def _updateBidder(self, _auctionId):
+        self._refundBid(_auctionId)
+        self.data.bids[_auctionId] = sp.record(
+            value = sp.amount, owner = sp.source
+        )
+
+    def _ownerOnly(self, add1, add2):
+        sp.verify(add1 == add2, self.error.ownerOnly())
+
+    @sp.private_lambda(with_storage = 'read-write')
+    def calculatePercentage(self, params):
+        sp.set_type(params,
+            sp.TRecord(percentage = sp.TNat, amount = sp.TNat
+        ))
+        value = (params.percentage * params.amount) // 1000
+        sp.result(value)
+
+    # Core Functions
     @sp.entry_point
     def setAdmin(self, params):
         self._onlyAdmin()
@@ -77,12 +160,122 @@ class marketplace(sp.Contract):
             thirdPartyAdmin = params.thirdPartyAdmin
         )
 
-    @sp.private_lambda(with_storage = 'read-write')
-    def calculatePercentage(self, params):
-        sp.set_type(params,
-            sp.TRecord(percentage = sp.TNat, amount = sp.TNat
+    @sp.entry_point
+    def createAuction(self, params):
+        sp.set_type(params,sp.TRecord(
+            token = sp.TAddress, tokenId = sp.TNat, amount = sp.TNat,
+            basePrice = sp.TMutez, timePeriod = sp.TInt
         ))
-        value = (params.percentage * params.amount) // 1000
-        sp.result(value)
+
+        sp.verify(sp.utils.mutez_to_nat(params.basePrice) > sp.nat(0),self.error.invalidPrice())
+        sp.verify(params.timePeriod > sp.int(0), self.error.invalidTimePeriod())
+
+        self._transferTokens(
+            params.token, 
+            params.tokenId, 
+            params.amount, 
+            sp.source,
+            sp.self_address
+        )
+
+        self.data.auctions[self.data.auctionId] = sp.record(
+            token = params.token, tokenId = params.tokenId, amount = params.amount,
+            basePrice = params.basePrice, seller = sp.source, timePeriod = sp.now.add_seconds(params.timePeriod)
+        )
+        
+        self.data.auctionId += 1
+
+    @sp.entry_point
+    def bid(self, _auctionId):
+        sp.set_type(_auctionId, sp.TNat)
+        self._auctionExists(_auctionId)
+        self._isAuctionActive(_auctionId)
+        self._isBidValid(_auctionId)
+
+        self._updateBidder(_auctionId)
+
+    @sp.entry_point
+    def cancelAuction(self, _auctionId):
+        sp.set_type(_auctionId, sp.TNat)
+        self._auctionExists(_auctionId)
+        self._ownerOnly(self.data.auctions[_auctionId].seller, sp.source)
+        self._refundBid(_auctionId)
+        self._transferTokens(
+            self.data.auctions[_auctionId].token, 
+            self.data.auctions[_auctionId].tokenId,
+            self.data.auctions[_auctionId].amount,
+            sp.self_address,
+            sp.source
+        )
+        del self.data.auctions[_auctionId]
+    
+    @sp.entry_point
+    def withDraw(self, _auctionId):
+        sp.set_type(_auctionId, sp.TNat)
+        self._auctionExists(_auctionId)
+        sp.if self.data.bids.contains(_auctionId):
+            currBid = sp.local('bid',self.data.bids[_auctionId])
+            currAuction = sp.local('auction', self.data.auctions[_auctionId])
+            sp.verify((sp.source == currBid.value.owner) | (sp.source == currAuction.value.seller),self.error.ownerOnly())
+            
+            del self.data.bids[_auctionId]
+            del self.data.auctions[_auctionId]
+
+            royalty = sp.local('royalty', self.calculatePercentage(sp.record(percentage = self.data.royalty, amount = sp.utils.mutez_to_nat(currBid.value.value))))
+            toTP = sp.local('toTP',0)
+            toUs = sp.local('toUs',royalty.value)
+            buyer = sp.local('buyer',currBid.value.owner)
+            seller = sp.local('seller',currAuction.value.seller)
+            toSeller = sp.utils.mutez_to_nat(currBid.value.value) - royalty.value
+
+            sp.if self.data.royaltyDivision.contains(self.data.auctions[_auctionId].token):
+                toTP.value = self.calculatePercentage(
+                    sp.record(
+                        percentage = self.data.royaltyDivision[self.data.auctions[_auctionId].token].toThirdParty, 
+                        amount = royalty.value
+                    )
+                )
+                toUs.value = self.calculatePercentage(
+                    sp.record(
+                        percentage = self.data.royaltyDivision[self.data.auctions[_auctionId].token].toAdmin, 
+                        amount = royalty.value
+                    )
+                )
+
+            self._transferTokens(
+                currAuction.value.token, 
+                currAuction.value.tokenId,
+                currAuction.value.amount,
+                sp.self_address,
+                buyer.value
+            )
+            
+            sp.send(
+                self.data.royaltyDivision[self.data.auctions[_auctionId].token].thirdPartyAdmin, 
+                sp.utils.nat_to_mutez(toTP.value)
+            )
+            sp.send(
+                self.data.admin,
+                sp.utils.nat_to_mutez(toUs.value)
+            )
+            sp.send(
+                seller.value,
+                sp.utils.nat_to_mutez(sp.as_nat(toSeller))
+            )
+            
+        sp.else :
+            currAuction = sp.local('auction', self.data.auctions[_auctionId])
+            sp.verify(sp.source == currAuction.value.seller,self.error.ownerOnly())
+            
+            del self.data.auctions[_auctionId]
+
+            self._transferTokens(
+                currAuction.value.token, 
+                currAuction.value.tokenId,
+                currAuction.value.amount,
+                sp.self_address,
+                currAuction.value.seller
+            )
 
 
+        
--- test/marketplace_test.py
+++ test/marketplace_test.py
@@ -3,6 +3,40 @@ import smartpy as sp
 Marketplace = sp.io.import_stored_contract("marketplace")
 fa2 = sp.io.import_stored_contract("fa2")
 
+def mintNFT(nft, id, to, admin):
+    tok0_md = fa2.FA2.make_metadata(
+            name = "The Token One",
+            decimals = 0,
+            symbol= "TK0" )
+    nft.mint(
+        address = to,
+        amount = 1,
+        metadata = tok0_md,
+        token_id = id
+    ).run(sender = admin)
+
+def mintFT(ft, id, to, admin, amount):
+    tok0_md = fa2.FA2.make_metadata(
+            name = "The Token One",
+            decimals = 6,
+            symbol= "TK0" )
+    ft.mint(
+        address = to,
+        amount = amount,
+        metadata = tok0_md,
+        token_id = id
+    ).run(sender = admin)
+
+def setOperator(token, owner, operator, id):
+        token.update_operators([
+                sp.variant("add_operator", token.operator_param.make(
+                    owner = owner.address,
+                    operator = operator.address,
+                    token_id = id
+                    )
+                ),
+            ]).run(sender = owner)
+
 @sp.add_test(name = "GameGeeks Marketplace")
 def test():
     scenario = sp.test_scenario()
@@ -19,9 +53,30 @@ def test():
             admin = admin1.address
         )
 
+    nft2 = fa2.FA2(
+            config = fa2.FA2_config(non_fungible = True),
+            metadata = sp.utils.metadata_of_url("https://example.com"),
+            admin = admin1.address
+        )
+
+    ft1 = fa2.FA2(
+            config = fa2.FA2_config(non_fungible = False),
+            metadata = sp.utils.metadata_of_url("https://example.com"),
+            admin = admin1.address
+        )
+
+    ft2 = fa2.FA2(
+            config = fa2.FA2_config(non_fungible = False),
+            metadata = sp.utils.metadata_of_url("https://example.com"),
+            admin = admin1.address
+        )
+
     c = Marketplace.marketplace(admin = admin1.address,_royalty =  sp.nat(1))
     scenario += c
     scenario += nft1
+    scenario += nft2
+    scenario += ft2
+    scenario += ft1
 
     # ---------------- Set admin ---------------- #
     
@@ -68,4 +123,87 @@ def test():
         toThirdParty = 300,
         toAdmin = 700
     )).run(sender = admin2)
+
+    # ----------------- Create New Auction --------------
+
+    mintNFT(nft1, 0, admin1.address, admin1)
+    mintNFT(nft2, 0, user1.address, admin1)
+    mintFT(ft1, 0, admin1.address, admin1, 100000000)
+
+    # it should not create new auction if base price is 0
+    c.createAuction(sp.record(
+        token = nft1.address, tokenId = 0, amount = sp.nat(1),
+        basePrice = sp.mutez(0), timePeriod = sp.int(86400)
+    )).run(sender = admin1, valid = False)
+
+    # it should not create new auction if time period if 0
+    c.createAuction(sp.record(
+        token = nft1.address, tokenId = 0, amount = sp.nat(1),
+        basePrice = sp.mutez(100000), timePeriod = sp.int(0)
+    )).run(sender = admin1, valid = False)
+
+    # it should not create new auction if not called by the token owner
+    c.createAuction(sp.record(
+        token = nft1.address, tokenId = 0, amount = sp.nat(1),
+        basePrice = sp.mutez(100000), timePeriod = sp.int(86400)
+    )).run(sender = user1, valid = False)
+
+    # it should create new auction with NFT, and transfer token to the vault
+    setOperator(nft1, admin1, c, 0)
+    c.createAuction(sp.record(
+        token = nft1.address, tokenId = 0, amount = sp.nat(1),
+        basePrice = sp.mutez(100000), timePeriod = sp.int(86400)
+    )).run(sender = admin1)
+
+    scenario.verify(nft1.data.ledger[nft1.ledger_key.make(c.address, 0)].balance == 1)
+    scenario.verify(nft1.data.ledger[nft1.ledger_key.make(admin1.address, 0)].balance == 0)
+
+    # it should create new auction with FT, and transfer token to the vault
+    setOperator(ft1, admin1, c, 0)
+    c.createAuction(sp.record(
+        token = ft1.address, tokenId = 0, amount = sp.nat(100000),
+        basePrice = sp.mutez(100), timePeriod = sp.int(86400)
+    )).run(sender = admin1)
+
+    scenario.verify(ft1.data.ledger[ft1.ledger_key.make(c.address, 0)].balance == 100000)
+
+
+    # ----------------- New Bid --------------
+
+    # it should not make bid if auction does not exist
+    c.bid(sp.nat(3)).run(sender = user2, amount = sp.mutez(100000), valid = False)
+
+    # it should not make bid if auction is expired
+
+    # it should not make bid if 1st bid is less than base price
+    c.bid(sp.nat(0)).run(sender = user1, amount = sp.mutez(10), valid = False)
+
+    # it should make the first bid
+    c.bid(sp.nat(0)).run(sender = user1, amount = sp.mutez(100000))
+
+    # it should not make bid if it is less than current bid
+    c.bid(sp.nat(0)).run(sender = user2, amount = sp.mutez(1000), valid = False)
+
+    # it should make a bid, refund the previoud bidder and update the bid details
+    c.bid(sp.nat(0)).run(sender = user2, amount = sp.mutez(1000000))    
+
+
+    # --------------- Cancel Auction -----------
+
+    # it should not cancel auction if it does not exist
+    c.cancelAuction(5).run(sender = admin2, valid = False)
+
+    # it should not cancel auction if not called by the auction owner
+    c.cancelAuction(0).run(sender = user1, valid = False)
+
+    # it should cancel auction, refund the bid if any, return the assets to the owner and delete this entry from the map
+    c.cancelAuction(0).run(sender = admin1)
+    scenario.verify(nft1.data.ledger[nft1.ledger_key.make(c.address, 0)].balance == 0)
+    scenario.verify(nft1.data.ledger[nft1.ledger_key.make(admin1.address, 0)].balance == 1)
+
+
+    #---------------- Withdraw ----------------
+
+    
+
 
